1072e0e40c24e894622c784410f42d2507f6b3cb
Varun Joshi <v9joshi@gmail.com>
2020-12-04 02:03:28 -0500

    day4
    playing with regular expressions

---
+++ Day4/processDay4.py
@@ -0,0 +1,89 @@
+import csv
+import re
+
+def checkValidity(inputDict):
+    # years
+    byr = inputDict['byr']
+    byrPattern = re.compile("19[2-9][0-9]|200[0-2]")
+
+    iyr = inputDict['iyr']
+    iyrPattern = re.compile("201[0-9]|2020")
+
+    eyr = inputDict['eyr']
+    eyrPattern = re.compile("202[0-9]|2030")
+
+    # height
+    hgt = inputDict['hgt']
+    hgtPattern = re.compile("59in|6[0-9]in|7[0-6]in|1[5-8][0-9]cm|19[0-3]cm")
+
+    # hair/eye color
+    hcl = inputDict['hcl']
+    hclPattern = re.compile("#[0-9A-Fa-f]{6}")
+
+    ecl = inputDict['ecl']
+    eclPattern = re.compile("amb|blu|brn|gry|grn|hzl|oth")
+
+    # passport id
+    pid = inputDict['pid']
+    pidPattern = re.compile("[0-9]{9}")
+
+    if not byrPattern.fullmatch(byr):
+        return 0
+
+    if not iyrPattern.fullmatch(iyr):
+        return 0
+
+    if not eyrPattern.fullmatch(eyr):
+        return 0
+
+    if not hgtPattern.fullmatch(hgt):
+        return 0
+
+    if not hclPattern.fullmatch(hcl):
+        return 0
+
+    if not eclPattern.fullmatch(ecl):
+        return 0
+
+    if not pidPattern.fullmatch(pid):
+        return 0
+
+    return 1
+
+# create an empty array
+inputList = []
+# store the input in the array
+with open('input.txt',newline='') as csvfile:
+    inputReader = csv.reader(csvfile, delimiter= ' ', quotechar='|')
+    for row in inputReader:
+        inputList.append(row)
+
+# merge the broken segments of the list to make individual entries for a person
+newList = [[]]
+for currVal in inputList:
+    if currVal:
+        newList[-1].extend(currVal)
+    else:
+        newList.append([])
+
+# initialize checking variables
+numRight = 0
+numWrong = 0
+
+checkSet = set(['byr','iyr','eyr','hgt','hcl','ecl','pid'])
+# now check each entry
+dictList = []
+for currPass in newList:
+    # check each value in each entry
+    currDict = {currVal.split(':')[0]:currVal.split(':')[1] for currVal in currPass}
+    dictList.append(currDict)
+    if checkSet.issubset(currDict):
+        if checkValidity(currDict):
+            numRight = numRight + 1
+        else:
+            numWrong = numWrong + 1
+    else:
+        #print('Invalid', currDict)
+        numWrong = numWrong + 1
+
+print(numRight, ' valid passports, and ', numWrong,' invalid passports')
