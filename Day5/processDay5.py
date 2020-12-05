652c0ec32aa39c90084644beabe6509ed6144cb8
Varun Joshi <v9joshi@gmail.com>
2020-12-05 00:59:24 -0500

    Added comments
    Added some comments to the code.

--- Day5/processDay5.py
+++ Day5/processDay5.py
@@ -1,19 +1,23 @@
 import csv
 
 def codeToNum(seatCode):
+    # parse the seat code
     rowCode = seatCode[0:7]
+    # row values are stored as binary with F = 0,  = 1
     rowCode = rowCode.replace('F','0')
     rowCode = rowCode.replace('B','1')
 
+    # Column values are stored as binary with L = 0, R = 1
     colCode = seatCode[7:10]
     colCode = colCode.replace('L','0')
     colCode = colCode.replace('R','1')
 
+    # Convert the binary numbers into integers
     rowNum = int(rowCode,2)
     colNum = int(colCode,2)
 
-    #return [rowCode, colCode]
-    return rowNum*8 +colNum
+    # return the seat number
+    return rowNum*8 +colNum # We could also do this without splitting row and col
 
 # create an empty array
 inputList = []
@@ -23,12 +27,22 @@ with open('input.txt',newline='') as csvfile:
     for row in inputReader:
         inputList.append(row[0])
 
+# create an empty list of seats
 seatNum = []
 
+# Find out all the seats numbers from the codes
 for seatCode in inputList:
     seatNum.append(codeToNum(seatCode))
 
+# First half
+print('Highest seat number ', max(seatNum))
+
+# Second half
+# Create a list of all seats
 allSeats = list(range(min(seatNum),max(seatNum),1))
+
+# Find the missing seat using a set difference
 missingSeats = list(set(allSeats)-set(seatNum))
 
-print(missingSeats)
+# Print the missing seats
+print('Unassigned seats: ', missingSeats)
