9ff525fd84c0de57ae5cc716d872dab678da42bc
Varun Joshi <v9joshi@gmail.com>
2020-12-15 12:53:00 -0500

    Comment update
    Added comments to improve clarity.

--- Day15/processDay15.py
+++ Day15/processDay15.py
@@ -11,6 +11,7 @@ with open('input.txt',newline='') as csvfile:
         inputRow = [int(rowVal) for rowVal in row]
         inputList.extend(inputRow)
 
+# First half: van eck's sequence
 numValList = inputList.copy()
 numValList.reverse()
 
@@ -28,27 +29,41 @@ print('2020th spoken number is: ', numValList[0])
 
 # Second half
 # Don't store everything to save on memory
+# Hash table implementation
 inputKeys = set(inputList)
 inputDict = {}
 
+# Table initialization
 for inputKey in inputKeys:
     inputDict[inputKey] = inputList.index(inputKey)
 
+# Go back a bit sice we have to treat the last entry as the current input
 newInput = inputList[-1]
 inputCount = len(inputList) - 1
 
+# Keep going till we have 30 million inputs
 while inputCount < 30000000:
+    # The new input is the last input
     lastInput = newInput
 
+    # Check for presence in the hash table
     if lastInput in inputKeys:
+        # If present, New input is the distance to occurence
         newInput = inputCount - inputDict[lastInput]
+
+        # Update the hash table to note that the last occurence was now
         inputDict[lastInput] = inputCount
     else:
+        # If not present, add the input to the table
         inputDict[lastInput] = inputCount
         inputKeys.add(lastInput)
+
+        # Since the input has never occurred, the new input is 0
         newInput = 0
 
+    # Update the input count
     inputCount = inputCount + 1
 
-
-print('2020th spoken number is: ', lastInput)
+# Find the 30 millionth number
+print('30000000th spoken number is: ', lastInput)
+print('The hash table has: ', len(inputKeys), 'entries')
