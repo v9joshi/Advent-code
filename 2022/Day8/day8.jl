7e2f9dfca2c2588ec8ca3c27ffb3e11fc62f7ced
Varun Joshi <joshi.142@osu.edu>
2022-12-08 14:45:07 +0530

    Day 8 finished
    Day 8 completed

---
+++ 2022/Day8/day8.jl
@@ -0,0 +1,56 @@
+# Read inputs
+inputs = read(".\\input.txt", String) # Read all the inputs
+inputs = replace(inputs,'\r'=>"")     # Sanitize inputs
+inputs = strip(inputs,'\n')           # Trailing end line is annoying
+inputs = split(inputs,'\n')           # Split into lines
+
+# Convert inputs into a matrix
+treeMatrix = hcat(map(x -> parse.(Int,x),split.(inputs,""))...)
+
+# What is the matrix size?
+(maxX, maxY) = size(treeMatrix)
+
+# Make a visibility dictionary
+visibleDict = Dict()
+scenicScore = Dict()
+
+# Find the number of trees visible from current tree
+function SceneCheck(currTree, otherTrees)
+    index = findfirst(x -> x ≥ currTree, otherTrees)
+
+    # Check if the tree is being blocked, and if yes by how many trees
+    if isnothing(index)
+        return (length(otherTrees), 0)
+    else
+        return (index[1], 1)
+    end
+end
+
+for (idx, idy) in Iterators.product(2:maxX-1,2:maxY-1)
+    # Get the trees to the left and right
+    currLeft  = reverse(treeMatrix[1:idx-1,idy])
+    currRight = treeMatrix[idx+1:end,idy]
+
+    # Get the trees to the top and bottom
+    currTop   = reverse(treeMatrix[idx,1:idy-1])
+    currBot   = treeMatrix[idx,idy+1:end]
+
+    # Get current element
+    currEle = treeMatrix[idx, idy]
+
+    # Find first blocking tree in each direction
+    (scoreLeft,  visLeft)  = SceneCheck(currEle, currLeft)
+    (scoreRight, visRight) = SceneCheck(currEle, currRight)
+    (scoreTop,   visTop)   = SceneCheck(currEle, currTop)
+    (scoreBot,   visBot)   = SceneCheck(currEle, currBot)
+
+    # Find the scenic score and the visibility
+    scenicScore[(idx, idy)] = scoreLeft*scoreRight*scoreTop*scoreBot
+    visibleDict[(idx,idy)]  = visLeft*visRight*visTop*visBot
+end
+
+# How many trees are visible?
+println("Num visible = ", maxX*maxY - (map(x -> x == 1, values(visibleDict)) |> sum ))
+
+# What's the max scenic score?
+println("Max score = ", maximum(values(scenicScore)))
--- 2022/README.md
+++ 2022/README.md
@@ -3,10 +3,11 @@ https://adventofcode.com/2022 More Julia
 
 | Day | Challenge | Main tool |
 | --- | --------- |-----------|
-| 01  | Calorie Counter  | partialSortPerm |
-| 02  | Rock paper scissors  | dictionaries |
+| 01  | Calorie Counter  | PartialSortPerm |
+| 02  | Rock paper scissors  | Dictionaries |
 | 03  | Checking bags  | Maps |
 | 04  | Overlaping areas  | Set functions |
 | 05  | Operate crane  | Regex |
-| 06  | Signal transcription  | functions |
-| 07  | File system  | struct |
+| 06  | Signal transcription  | Functions |
+| 07  | File system  | Struct |
+| 08  | Tree house  | Matrices |
