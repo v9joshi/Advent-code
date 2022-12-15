5a6faf754479468b988592035e68e9afa98100ec
Varun Joshi <joshi.142@osu.edu>
2022-12-15 18:31:10 +0530

    Update day15
    Added 2nd version of part 2, much faster.

--- 2022/Day15/day15.jl
+++ 2022/Day15/day15.jl
@@ -89,28 +89,94 @@ dt3 = time() - t
 println("Time taken = $dt1, $dt2, $dt3")
 println("Num blocked points = ", blockedPoints)
 
-# Part 2
+# Part 2 version 1
+# t = time()
+# maxInd     = 4000000
+# minInd     = 0
+# tuningFreq = 0
+# for currRow in minInd:maxInd
+#     # Find all the blocked points
+#     currBlocked    = findBlocked(currRow, sensorDict, minInd, maxInd)
+#     if length(currBlocked) == 2
+#         viablePoints = min(currBlocked[1][2], currBlocked[2][2]) + 1: max(currBlocked[1][1], currBlocked[2][1]) - 1
+#     elseif length(currBlocked) == 1
+#         viablePoints = union([minInd:currBlocked[1][1]],[currBlocked[1][2]:maxInd])
+#     else
+#         continue
+#     end
+        
+#     if length(viablePoints) == 1
+#         global tuningFreq = only(viablePoints)*4000000 + currRow
+#         break
+#     end
+# end
+
+# dt4 = time() - t
+# println("Time taken = $dt4")
+# println("Tuning freq: ", tuningFreq)
+
+# Part 2 version 2
 t = time()
 maxInd     = 4000000
 minInd     = 0
 tuningFreq = 0
-for currRow in minInd:maxInd
-    # Find all the blocked points
-    currBlocked    = findBlocked(currRow, sensorDict, minInd, maxInd)
-    if length(currBlocked) == 2
-        viablePoints = min(currBlocked[1][2], currBlocked[2][2]) + 1: max(currBlocked[1][1], currBlocked[2][1]) - 1
-    elseif length(currBlocked) == 1
-        viablePoints = union([minInd:currBlocked[1][1]],[currBlocked[1][2]:maxInd])
-    else
-        continue
-    end
-        
-    if length(viablePoints) == 1
-        global tuningFreq = only(viablePoints)*4000000 + currRow
-        break
+
+linesStoreA = []
+linesStoreB = []
+for (sx, sy) in keys(sensorDict)
+    # Each sensor has 4 lines marking its detection boundary
+    distVal = distance((sx, sy), sensorDict[(sx,sy)]) + 1
+
+    # For a sensor at location sx, sy
+    # These lines are given by -
+    # 1. (y - sy) =  -(x - sx) + distval
+    # 2. (y - sy) =  -(x - sx) - distval
+    # 3. (y - sy) =   (x - sx) + distval
+    # 4. (y - sy) =   (x - sx) - distval
+    # Store the slope and intercept for these lines
+    linesA = [(-1, sx + sy + distVal), (-1, sx + sy - distVal)]
+    linesB = [(1, -sx + sy + distVal), (1, -sx + sy - distVal)]
+
+    # Add this to the set of lines we already have
+    append!(linesStoreA, linesA)
+    append!(linesStoreB, linesB)
+end
+
+# Find the points of intersection for these lines
+intersectionPoints = []
+for (line1, line2) in Iterators.product(linesStoreA,linesStoreB)
+    # Two lines with y = m1*x + c1 and y = m2*x + c2, intersect at
+    # y = (m1*c2 - m2*c1)/(c2 - c1) and x = (c2 - c1)/(m1 - m2) 
+    currX = (line2[2] - line1[2])/(line1[1] - line2[1])
+    currY = currX*line2[1] + line2[2]
+
+    # Append this point to the list
+    if (0 ≤ currX ≤ maxInd) && (0 ≤ currY ≤ maxInd) && isinteger(currX) && isinteger(currY)
+        push!(intersectionPoints, (currX, currY))
     end
 end
 
+# Check these points to see if any of them are the right point
+for point in intersectionPoints
+    detectable = true
+
+    for sensor in keys(sensorDict)
+        detectableBySensor  = distance(sensor, point) > distance(sensor,sensorDict[sensor])
+        
+        if detectableBySensor
+            detectable = true
+        else
+            detectable = false
+            break
+        end
+    end
+
+    if detectable
+        global tuningFreq = point[1]*4000000 + point[2]
+    end        
+end
+
+# Print the answer
 dt4 = time() - t
 println("Time taken = $dt4")
 println("Tuning freq: ", tuningFreq)
